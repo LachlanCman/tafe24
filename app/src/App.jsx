cc351b546e0b69d2bdfdbf95083cc382a785a1b5
LachlanCman <lachlan.cashion@studytafensw.edu.au>
2024-09-16 10:55:44 +0000

    added bootstrap and firebase config

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,37 +1,55 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import ting from './assets/Screenshot (4).png'
+import "bootstrap/dist/css/bootstrap.css"
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://www.youtube.com/watch?v=09gj5gM4V98&t=3241s" target="_blank">
-          <img src='https://www.youtube.com/watch?v=09gj5gM4V98&t=3241s' className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={ting} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <>
+     <nav className="navbar navbar-expand-lg bg-dark">
+   <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span classNameName="navbar-toggler-icon"></span>
+    </button>
+    <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
+        <li classNameName="nav-item">
+          <a classNameName="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
     </>
   )
-}
 
+}
 export default App
